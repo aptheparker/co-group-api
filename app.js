9b85b9b5342c8ede7cf7c0c63badeeb1fc6b97e8
aptheparker <apapap1119@g.skku.edu>
2023-11-28 23:20:44 +0900

    swagger added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 
+const { swaggerUi, specs } = require("./swagger/swagger")
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
+
 const authRoutes = require("./routes/AuthRoute");
 const userRoutes = require("./routes/UserRoute");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: 유저 추가 수정 삭제 조회
+ */
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -3,7 +3,54 @@ const router = express.Router();
 
 const UserController = require("../controllers/UserController");
 
+/**
+ * @swagger
+ *  /:userId:
+ *    get:
+ *      summary: 사용자 정보 조회
+ *      tags:
+ *      - user
+ *      description: 사용자 정보 조회
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: param
+ *          name: userId
+ *          required: true
+ *          schema:
+ *            type: string
+ *            description: 유저 아이디
+ *      responses:
+ *       200:
+ *        description: 사용자 정보 조회 성공
+ */
 router.get("/", UserController.getUser);
+
+/**
+ * @swagger
+ *  /:userId:
+ *    put:
+ *      summary: 사용자 정보 수정
+ *      tags:
+ *      - user
+ *      description: 사용자 정보 수정
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: param
+ *          name: userId
+ *          required: true
+ *        - in: body
+ *          name: name
+ *          required: false
+ *          schema:
+ *            type: string
+ *            description: 유저 이름
+ *      responses:
+ *       200:
+ *        description: 사용자 정보 수정 성공
+ */
+
 router.put("/", UserController.updateUser);
 
 module.exports = router;
